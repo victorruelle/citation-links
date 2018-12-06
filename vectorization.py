b068db6ed94808d7c43c0297536381fccf785bb5
Josephine Yates <josephine.yates@polytechnique.edu>
2018-12-06 17:01:18 +0100

    Modif_061_yes

--- vectorization.py
+++ vectorization.py
@@ -1,6 +1,7 @@
 import csv
 from collections import defaultdict
 from gensim import corpora, models, similarities
+from sklearn.linear_model import LogisticRegression
 #activate log 
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -73,6 +74,44 @@ index2 = similarities.MatrixSimilarity(corpus_titles_lsi)
 #here we compute the similarity between all the documents and the first document
 sims1 = index1[corpus_abstract_lsi]
 sims2 = index2[corpus_titles_lsi]
+
+list_sims1 = list(enumerate(sims1))
+list_sims2 = list(enumerate(sims2))
 #list(enumerate(sims1 or sims2)) gives a list of tuples (index of doc, array[(index of doc, similarity)])
 #list(enumerate(sims1 or sims2))[i][1] is the array of similarities for doc i 
+
+#initialisation de la liste de listes contenant l'info booléenne: les documents sont-ils liés ou non?
+Y = [[0]*len(list_sims1[0])]*len(list_sims1[0])
+for j in range(len(training_set)):
+#remplir de façon symétrique
+	Y[int(training_set[j][1])][int(training_set[j][2])] = int(training_set[j][3])
+	Y[int(training_set[j][2])][int(training_set[j][1])] = int(training_set[j][3])
+#maintenant, Y[i][j] == 1 si les docs i et j sont liés, 0 sinon
+
+#models_XXX[i] contiendra le modèle de régression logistique adaptée au document i pour la caractéristique XXX (titles or abstract)
+models_abstract = []
+models_titles = []	
+for i in range(len(list_sims1[0])):
+	Xi1 = list_sims1[i][1]
+	Xi2 = list_sims2[i][1]
+	Yi = Y[i]
+	
+	models_abstract.append(LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(Xi1, Yi))
+	models_titles.append(LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(Xi2, Yi))
+
+#on process le testing set pour qu'il soit sous forme de similarités
+test_sims_abstract = []
+test_sims_titles = []
+
+for j in range(len(testing_set)):
+	test_sims_abstract.append(list_sims1[int(testing_set[j][1])][int(testing_set[j][2])])
+	test_sims_titles.append(list_sims2[int(testing_set[j][1])][int(testing_set[j][2])])	
+
+#predictions_abstract[i] et predictions_titles[i] vont contenir les predicitions pour le testing set en fonction du modèle adapté au document i 
+predictions_abstract = []
+predictions_titles = []
+for i in range(len(list_sims1[0])):
+	predictions_abstract.append(models_abstract[i].predict(test_sims_abstract))
+	predictions_titles.append(models_titles[i].predict(test_sims_titles))
+	
 
