6352fe103332f0eee20e0201b20098cae214c15b
Josephine Yates <josephine.yates@polytechnique.edu>
2018-12-25 13:02:47 +0100

    modif_Jo

---
+++ NN.py
@@ -0,0 +1,47 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+#load training data
+X_train = []
+Y_train = []
+
+#load testing data
+X_test = []
+Y_test = []
+
+# create model
+model = Sequential()
+#change numbers and number of features
+#Dense is fully connected, maybe make it half connected?
+#change number of layers
+model.add(Dense(12, input_dim=0, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile model
+#use adaboost as an optimizer? need further investigation
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X_train, Y_train, epochs=150, batch_size=10)
+
+# evaluate the model
+#scores = model.evaluate(X, Y)
+#print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+# calculate predictions
+predictions = model.predict(X_test)
+# round predictions
+rounded = [round(x[0]) for x in predictions]
+
+#calcule accuracy 
+accuracy = 0
+for i in range(len(predictions)):
+	if(predictions[i]==Y_test[i]):
+		accuracy+=1
+accuracy/=len(predictions)
+print("Accuracy : ",accuracy)	
