9ebd03d99c1a785beccfcef5acc79299c662d526
Nicolas ZUCCHET <nicolas.zucchet@polytechnique.edu>
2018-12-27 20:40:20 +0100

    Added information on features in main to help data mining

--- main.py
+++ main.py
@@ -112,19 +112,20 @@ def save_predictions(Y):
     print("prediction successfully written to Predictions/"+name)
     return name
 
-def log_predictions(file_name,params,accuracy):
+def log_predictions(file_name,general_params,method_params,accuracy):
     log_file = "Predictions/log_predictions.txt"
-    headers = "accuracy,prediction_file,params\n"
+    headers = "accuracy;prediction_file;general_params;method_params\n"
     if not os.path.isfile(log_file):
         open(log_file,"w").write(headers)
+    # the result is added to the log file to keep a trace of every test
     file = open(log_file,"a")
-    file.write("%f;%s;%s\n" % (accuracy,file_name,str(params)))
+    file.write("%f;%s;%s;%s\n" % (accuracy,file_name,str(general_params),str(method_params)))
 
-def test(params,X_training,y_training,X_validation,y_validation,X_testing):
+def test(general_params,method_params,X_training,y_training,X_validation,y_validation,X_testing):
     # test a given set of params and save the output
-    assert "method" in params.keys()
-    n_training = params["n_training"]
-    n_validation = params["n_validation"]
+    assert "method" in general_params.keys()
+    n_training = general_params["n_training"]
+    n_validation = general_params["n_validation"]
     # takes random part of the data
     permutation_training = np.random.permutation(len(X_training))[:n_training]
     permutation_validation = np.random.permutation(len(X_validation))[:n_validation]
@@ -132,18 +133,46 @@ def test(params,X_training,y_training,X_validation,y_validation,X_testing):
     y_training = y_training[permutation_training]
     X_validation = X_validation[permutation_validation,:]
     y_validation = y_validation[permutation_validation]
-    print(params)
-    if params["method"] == "NN":
-        size_layers = params["size_layers"]
-        epochs = params["epochs"]
+    print(general_params)
+    print(method_params)
+    # For each method, create the model with the specific parameters
+    if general_params["method"] == "NN":
+        size_layers = method_params["size_layers"]
+        epochs = method_params["epochs"]
         model = pred.NNClassfier(X_training.shape[1],size_layers)
         model.fit(X_training,y_training,epochs=epochs)
+    if general_params["method"] == "SVC":
+        gamma = method_params["gamma"]
+        selected_features = method_params["selected_features"]
+        model = pred.SVMClassifier(gamma=gamma,features=selected_features)
+        model.fit(X_training,y_training)
+    # Test the model, saves a prediction for the testing set and log everything
     accuracy = model.accuracy(X_validation,y_validation)
     print(accuracy)
     pred_Y = model.predict(X_testing)
     pred_file = save_predictions(pred_Y)
-    log_predictions(pred_file,params,accuracy)
+    log_predictions(pred_file,method_params,general_params,accuracy)
 
+# id, what it is, keep it
+
+
+features_info = [
+    [0,"(Graph) Adamic adar",True],
+    [1,"(Graph) Adjusted rand",True],
+    [2,"(Graph) Common Neighbors",True],
+    [3,"(Graph) Neighborhood distance",True],
+    [4,"(Graph) Neighbors measure",True],
+    [5,"(Graph) Prefential attachment",True],
+    [6,"(Graph) Ressource allocation",True],
+    [7,"(Graph) Same community",True],
+    [8,"(Graph) Total neighbors",True],
+    [9,"(Graph) U degree",True],
+    [10,"(Graph) V degree",True],
+    [11,"(Text) Abstract similitude",True],
+    [13,"(Text) Title similitude",True],
+    [14,"(Meta) Delta publication year",True],
+    [15,"(Meta) Number of common authors",True]
+]
 
 if (__name__ == "__main__"):
     X_training = np.array(vect.get_features_of_set("training",metas))
@@ -151,11 +180,10 @@ if (__name__ == "__main__"):
     X_validation = np.array(vect.get_features_of_set("validation",metas))
     y_validation = np.array([e[2] for e in validation_set])
     X_testing = np.array(vect.get_features_of_set("testing",metas))
-    params = {"method":"NN","n_training":100000,"n_validation":5000,"epochs":1}
-    for n_1 in range(2,20,5):
-        for n_2 in range(2,20,5):
-            params["size_layers"] = [n_1,n_2]
-            test(params,X_training,y_training,X_validation,y_validation,X_testing)
+    general_params = {"method":"SVC","n_training":20000,"n_validation":3000}
+    for gamma in np.arange(0.0005,0.005,0.0005):
+        method_params = { "gamma" : gamma, "selected_features" : "all"}
+        test(general_params,method_params,X_training,y_training,X_validation,y_validation,X_testing)
 
     # parameters to
 
